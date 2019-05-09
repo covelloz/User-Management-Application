6b1735dc3019e29f3c95affe0914c5834bd4f717
Michael Covello <covelloz.michael@gmail.com>
2019-05-08 22:13:58 -0700

    Configure database session and logger

--- uman/__init__.py
+++ uman/__init__.py
@@ -2,12 +2,13 @@ import logging
 from flask import Flask
 from uman.config import config
 from uman.blueprint import api
+from uman.models import Base, Engine, Session
 
 __version__ = '0.1'
 __author__ = 'Michael Covello'
 
 # configure logging
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('sqlalchemy.engine')
 logger.setLevel(logging.INFO)
 f_handler = logging.FileHandler(config.log_name, encoding='utf-8')
 f_format = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')
@@ -17,6 +18,9 @@ logger.addHandler(f_handler)
 # configure flask
 app = Flask(__name__)
 app.register_blueprint(api, url_prefix='/uman')
+# configure database
+Base.metadata.create_all(Engine)
+session = Session()
 
 
 def run_app(config):
