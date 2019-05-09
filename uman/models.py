3f72787973c43fdd2eef976826c2158a121f34bd
Michael Covello <covelloz.michael@gmail.com>
2019-05-08 21:36:51 -0700

    Add boilerplate for database model

---
+++ uman/models.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+Engine = create_engine('sqlite:///:memory:', echo=True)
+Session = sessionmaker(bind=Engine)
+
+
+class User(Base):
+    __tablename__ = 'users'
+    user_id = Column(Integer, primary_key=True)
+    user_name = Column(String)
+    user_email = Column(String)
+    user_birthday = Column(Date)
+    user_zipcode = Column(Integer)
+
+    def get(self, user_name):
+        return {
+            'Name': self.user_name,
+            'Email': self.user_email,
+            'Birthday': self.user_birthday,
+            'ZipCode': self.user_zipcode
+        }
+
+    def post(self):
+        pass
+
+    def delete(self, user_name):
+        pass
