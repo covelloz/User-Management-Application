67d462d5856b9bc2a847c5855d3de49ca866bc5e
Michael Covello <covelloz.michael@gmail.com>
2019-05-10 03:23:43 -0700

    Add API routes in blueprint

--- uman/blueprint.py
+++ uman/blueprint.py
@@ -1,8 +1,46 @@
-from flask import Blueprint
+from datetime import datetime
+from flask import Blueprint, jsonify, request, abort
+from uman.models import User, UserAPI
 
-api = Blueprint('api', __name__)
+users = Blueprint('users', __name__)
 
 
-@api.route('/')
-def home():
-    return 'This will be homepage!'
+@users.route('/user/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user_api = UserAPI()
+    response = user_api.get(user_id)
+    return jsonify(response)
+
+
+@users.route('/add-user', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
+        try:
+            content = request.get_json()
+            new_user = User(
+                user_name=content['user_name'],
+                user_email=content['user_email'],
+                user_birthday=datetime.strptime(
+                    content['user_birthday'], '%m/%d/%Y'),
+                user_zipcode=content['user_zipcode'],
+            )
+            user_api = UserAPI()
+            response = user_api.post(new_user)
+            return jsonify(response)
+        except KeyError:
+            return {
+                'Content': {},
+                'Message': 'Bad Payload'
+            }
+    else:
+        abort(400)
+
+
+@users.route('/delete-user/<int:user_id>', methods=['GET', 'POST'])
+def delete_user(user_id):
+    if request.method == 'POST':
+        user_api = UserAPI()
+        response = user_api.delete(user_id)
+        return jsonify(response)
+    else:
+        abort(400)
