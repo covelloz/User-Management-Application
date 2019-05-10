406fe230222c26b037f625fdf0eba8b0cfc46f08
Michael Covello <covelloz.michael@gmail.com>
2019-05-10 03:23:15 -0700

    Add UserAPI model

--- uman/models.py
+++ uman/models.py
@@ -6,11 +6,14 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 Base = declarative_base()
-Engine = create_engine('sqlite:///:memory:', echo=True)
+Engine = create_engine('sqlite:///:memory:')
+
 Session = sessionmaker(bind=Engine)
+session = Session()
 
 
 class User(Base):
+    """ Database ORM """
     __tablename__ = 'users'
     user_id = Column(Integer, primary_key=True)
     user_name = Column(String)
@@ -18,16 +21,66 @@ class User(Base):
     user_birthday = Column(Date)
     user_zipcode = Column(Integer)
 
-    def get(self):
+
+class UserAPI(object):
+    """ API """
+    def get(self, user_id):
+        user = session.query(User).filter_by(
+            user_id=user_id).first()
+        if user is not None:
+            response = {
+                'Content': self._serialize(user),
+                'Message': ''
+            }
+            return response
+        else:
+            response = {
+                'Content': {},
+                'Message': 'No record for user_id={} exists'.format(user_id)
+            }
+            return response
+
+    def post(self, new_user):
+        user = session.query(User).filter_by(
+            user_name=new_user.user_name).first()
+        if user is None:
+            session.add(new_user)
+            session.commit()
+            response = {
+                'Content': self._serialize(new_user),
+                'Message': 'Record successfully added'
+            }
+            return response
+        else:
+            response = {
+                'Content': self._serialize(user),
+                'Message': 'User for {} already exists'.format(user.user_name)
+            }
+            return response
+
+    def delete(self, user_id):
+        user = session.query(User).filter_by(
+            user_id=user_id).first()
+        if user is not None:
+            session.delete(user)
+            session.commit()
+            response = {
+                'Content': {},
+                'Message': 'Record successfully deleted'
+            }
+            return response
+        else:
+            response = {
+                'Content': {},
+                'Message': 'No record for {} exists'.format(user_id)
+            }
+            return response
+
+    def _serialize(self, user):
         return {
-            'Name': self.user_name,
-            'Email': self.user_email,
-            'Birthday': self.user_birthday,
-            'ZipCode': self.user_zipcode
+            'Id': user.user_id,
+            'Name': user.user_name,
+            'Email': user.user_email,
+            'Birthday': user.user_birthday,
+            'ZipCode': user.user_zipcode
         }
-
-    def post(self, session):
-        session.add(self)
-
-    def delete(self, session):
-        session.delete(self)
